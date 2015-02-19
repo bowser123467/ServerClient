6a242efe4a5141f35ddb701de88ac6d33ed73a3e
Tyler <tylerhasman10@hotmail.com>
2015-02-18 16:50:21 -0800

    Added udp support

--- TheClient/src/me/tyler/client/Client.java
+++ TheClient/src/me/tyler/client/Client.java
@@ -9,44 +9,52 @@ import java.nio.ByteBuffer;
 public class Client {
 
 	private User me;
-	
-	public Client(String ip, int port){
+
+	public Client(String ip, int port) {
 		try {
 			me = new User(new Socket(InetAddress.getByName(ip), port));
-			System.out.println("Client connected to "+ip+":"+port);
+			System.out.println("Client connected to " + ip + ":" + port);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public User getMe(){
+
+	public User getMe() {
 		return me;
 	}
-	
+
 	private void run() {
-		while(me.isConnected()){
+		while (me.isConnected()) {
 			runOnce();
 		}
 	}
-	
-	public void runOnce(){
-		byte[] data = me.readData();
-		if(data != null){
-			if(data.length > 0){
+
+	public void runOnce() {
+		byte[] data = me.readData(true);
+		byte[] dataUdp = me.readData(false);
+		
+		if (data != null) {
+			if (data.length > 0) {
 				ByteBuffer buf = ByteBuffer.wrap(data);
 				PacketFactory.process(buf, this);
 			}
 		}
+		if(dataUdp != null){
+			if (dataUdp.length > 0) {
+				ByteBuffer buf = ByteBuffer.wrap(dataUdp);
+				PacketFactory.process(buf, this);
+			}
+		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Client client = new Client(Constants.IP_ADDRESS, Constants.PORT);
-		if(client.getMe() != null)
+		if (client.getMe() != null)
 			client.run();
 		else
 			System.err.println("Could not connect to server!");
 	}
-	
+
 }
--- TheServer/src/me/tyler/server/PacketFactory.java
+++ TheServer/src/me/tyler/server/PacketFactory.java
@@ -4,155 +4,188 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PacketFactory {
-	
+public class PacketFactory
+{
+
 	private static List<PacketHook> hooks = new ArrayList<PacketHook>();
-	
+
 	private static byte PACKET_HANDSHAKE = 1;
+
 	private static byte PACKET_LOGIN_STATUS = 2;
+
 	private static byte PACKET_CHAT_MESSAGE = 3;
+
 	private static byte PACKET_QUIT = 4;
+
 	private static byte PACKET_CHANGE_NAME = 5;
-	
-	public static byte[] getHandshakePacket(){
+
+	public static byte[] getHandshakePacket()
+	{
 		ByteBuffer buf = getLazyBuffer(8);
-		
+
 		buf.put(PACKET_HANDSHAKE);
 		buf.putInt(Constants.VERSION);
-		
-		return buf.array();		
+
+		return buf.array();
 	}
-	
+
 	/**
 	 * 
 	 * @param status 0 is success, 1 is fail, 2 is server full
 	 * 
 	 */
-	public static byte[] getLoginStatus(byte status){
+	public static byte[] getLoginStatus(byte status)
+	{
 		ByteBuffer buf = getLazyBuffer(4);
-		
+
 		buf.put(PACKET_LOGIN_STATUS);
 		buf.put(status);
-		
+
 		return buf.array();
 	}
-	
-	public static byte[] getChatMessagePacket(String message){
-		
+
+	public static byte[] getChatMessagePacket(String message)
+	{
+
 		final int MAX_LENGTH = 125;
-		
+
 		ByteBuffer buf = getLazyBuffer(128);
 		byte[] bytes = message.getBytes();
-		
-		if(bytes.length > MAX_LENGTH){
+
+		if (bytes.length > MAX_LENGTH)
+		{
 			byte[] shortBytes = new byte[MAX_LENGTH];
-			
-			for(int i = 0; i < shortBytes.length;i++){
+
+			for (int i = 0; i < shortBytes.length; i++)
+			{
 				shortBytes[i] = bytes[i];
 			}
-			
+
 			bytes = shortBytes;
-			
+
 		}
-		
+
 		buf.put(PACKET_CHAT_MESSAGE);
 		buf.put((byte) bytes.length);
 		buf.put(bytes);
-		
+
 		return buf.array();
 	}
-	
-	public static byte[] getNameChangePacket(String name){
+
+	public static byte[] getNameChangePacket(String name)
+	{
 		byte[] byteName = name.getBytes();
 		ByteBuffer buf = getLazyBuffer(20);
-		
+
 		buf.put(PACKET_CHANGE_NAME);
-		
-		if(byteName.length > Constants.MAX_NAME_LENGTH){
+
+		if (byteName.length > Constants.MAX_NAME_LENGTH)
+		{
 			byte[] shortBytes = new byte[Constants.MAX_NAME_LENGTH];
-			
-			for(int i = 0; i < shortBytes.length;i++){
+
+			for (int i = 0; i < shortBytes.length; i++)
+			{
 				shortBytes[i] = byteName[i];
 			}
-			
+
 			byteName = shortBytes;
 		}
-		
+
 		buf.put(byteName);
-		
+
 		return buf.array();
 	}
-	
-	private static ByteBuffer getLazyBuffer(int size){
-		
-		if(size <= 0){
+
+	private static ByteBuffer getLazyBuffer(int size)
+	{
+
+		if (size <= 0)
+		{
 			throw new IllegalArgumentException("size must be positive");
 		}
-		
-		if(size > Byte.MAX_VALUE){
-			throw new IllegalArgumentException("size cannot be larger than "+Byte.MAX_VALUE);
+
+		if (size > Byte.MAX_VALUE)
+		{
+			throw new IllegalArgumentException("size cannot be larger than " + Byte.MAX_VALUE);
 		}
-		
+
 		ByteBuffer buf = ByteBuffer.allocate(size);
-		
+
 		buf.put((byte) size);
-		
+
 		return buf;
 	}
 
-	public static void process(User user, ByteBuffer buf, Server server) {
-		
+	@SuppressWarnings("unused")
+	public static void process(User user, ByteBuffer buf, Server server)
+	{
+
 		byte size = buf.get();
 		byte packetId = buf.get();
-		
-		if(packetId == PACKET_HANDSHAKE){
-			
+
+		if (packetId == PACKET_HANDSHAKE)
+		{
+
 			int version = buf.getInt();
-			
-			if(version != Constants.VERSION){
-				user.sendData(getLoginStatus((byte) 1));
+
+			if (version != Constants.VERSION)
+			{
+				user.sendReliableData(getLoginStatus((byte) 1));
 				user.disconnect();
-			}else{
-				String name = "Default ("+user.getUniqueID()+")";
-				user.setUsername(name);
-				user.sendData(getNameChangePacket(name));
 			}
-			
-		}else if(packetId == PACKET_CHAT_MESSAGE){
-			
+			else
+			{
+				String name = "Default (" + user.getUniqueID() + ")";
+				user.setUsername(name);
+				user.sendReliableData(getNameChangePacket(name));
+			}
+
+		}
+		else if (packetId == PACKET_CHAT_MESSAGE)
+		{
+
 			byte length = buf.get();
 			byte[] messageBytes = new byte[length];
 			String message = null;
-			
+
 			buf.get(messageBytes);
-			
+
 			message = new String(messageBytes);
-			
-			System.out.println(user.getUsername()+": "+message);
-			
-		}else if(packetId == PACKET_QUIT){
+
+			System.out.println(user.getUsername() + ": " + message);
+
+		}
+		else if (packetId == PACKET_QUIT)
+		{
 			user.disconnect();
-		}else if(packetId == PACKET_CHANGE_NAME){
+		}
+		else if (packetId == PACKET_CHANGE_NAME)
+		{
 			byte[] nameBytes = new byte[Constants.MAX_NAME_LENGTH];
 			String name = null;
-			
+
 			buf.get(nameBytes);
 			name = new String(nameBytes).trim();
-			
+
 			user.setUsername(name);
-			
-		}else{
-			for(PacketHook hook : hooks){
-				if(hook.getPacketId() == packetId){
+
+		}
+		else
+		{
+			for (PacketHook hook : hooks)
+			{
+				if (hook.getPacketId() == packetId)
+				{
 					hook.onReceive(server, user, buf);
 				}
 			}
 		}
-		
+
 	}
 
-	public static void hook(PacketHook packetHook) {
-		
+	public static void hook(PacketHook packetHook)
+	{
+
 	}
-	
+
 }
